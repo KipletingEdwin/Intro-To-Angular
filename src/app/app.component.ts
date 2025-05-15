b89dc33082a50febd07db7c991b187d9458f5285
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-15 11:42:23 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RolesComponent } from './components/roles/roles.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,RolesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'hello-world';
+  title = 'Angular app';
 }
---
+++ src/app/components/roles/roles.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-roles',
+  imports: [],
+  templateUrl: './roles.component.html',
+  styleUrl: './roles.component.css'
+})
+export class RolesComponent {
+  //string, number, booolean,date,object,array,null,undefined
+  firstName: string = " Angular Tutorial"; 
+
+  angularVersion = " Version 18";
+
+  version: number = 18;
+
+  isActive: boolean = false;
+
+  currentDate : Date = new Date();
+
+  inputType: string = "checkbox";
+
+}
