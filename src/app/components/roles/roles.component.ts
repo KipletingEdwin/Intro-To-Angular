9e73aa444140b5441ff2b8c13d9a3adb520303a7
Kipleting Edwin <kipletingedwin4@gmail.com>
2025-05-16 10:29:33 +0100

    finalize data binding

--- src/app/components/roles/roles.component.ts
+++ src/app/components/roles/roles.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-roles',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './roles.component.html',
   styleUrl: './roles.component.css'
 })
@@ -19,5 +20,7 @@ export class RolesComponent {
   currentDate : Date = new Date();
 
   inputType: string = "checkbox";
+
+  selectedState: string = '';
 
 }
